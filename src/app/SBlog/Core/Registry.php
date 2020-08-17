ac41ddde1da85c65e3eae17862869cb31b095686
Zoolootooy <maxradjia.zp@gmail.com>
2020-08-17 23:51:00 +0200

    Added Singleton, Registry, BlogApp

---
+++ src/app/SBlog/Core/Registry.php
@@ -0,0 +1,41 @@
+<?php
+
+
+namespace App\SBlog\Core;
+
+
+class Registry
+{
+    use TSingletone;
+
+    protected static $properties = [];
+
+    /**
+     * @param $name
+     * @param $value
+     */
+    public function setProperty($name, $value)
+    {
+        self::$properties[$name] = $value;
+    }
+
+    /**
+     * @param $name
+     * @return mixed|null
+     */
+    public function getProperty($name)
+    {
+        if (isset(self::$properties[$name])) {
+            return self::$properties[$name];
+        }
+        return null;
+    }
+
+    /**
+     * @return array $properties
+     */
+    public function getProperties()
+    {
+        return self::$properties;
+    }
+}
